f5314f8e6dbac9baf67bff5710502f8ef7322f92
Chris Lewis <13chlewis@gmail.com>
2019-04-16 10:37:25 -0600

    Favorite location endpoint working

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -4,10 +4,10 @@ skip_before_action :verify_authenticity_token
     user = User.find_by(api_key: params[:favorite][:api_key])
     if user
       location = Location.find_or_create_by(name: params[:favorite][:location])
-      user.locations << location
-      render status: 201
+      user.locations << location unless user.locations.find_by(name: location.name)
+      render status: 201, json: { message: "You have favorited #{location.name}!"}
     else
-
+      render status: 404, json: { message: "There was an error with your request"}
     end
   end
 end
