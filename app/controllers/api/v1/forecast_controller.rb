99463047feffdaf3c2db99ea477d23694e83cbe2
Chris Lewis <13chlewis@gmail.com>
2019-04-22 09:42:26 -0600

    Removed unnecessary to_json methods

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ForecastController < Api::V1::BaseController
   def show
     lat, long = Geocoder.lat_long(params[:location])
     response = DarkSkyService.new.forecast(lat, long)
-    render status: 200, json: ForecastFacade.new(response).to_json
+    render status: 200, json: ForecastFacade.new(response)
   end
 end
